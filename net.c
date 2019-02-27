601f0ed9365dee18aa9ed3f147b5424c17c6a16b
codinghuang <2812240764@qq.com>
2019-02-27 18:58:01 +0800

    Feat: add tswCo_recv and tswCo_send

--- net.c
+++ net.c
@@ -7,6 +7,26 @@
 #include "log.h"
 #include "epoll.h"
 
+static int tswCo_setnonblock(int fd);
+
+static int tswCo_setnonblock(int fd)
+{
+    int flags;
+
+    flags = fcntl(fd, F_GETFL, 0);
+    if (flags < 0) {
+        tswWarn("%s", strerror(errno));
+        return TSW_ERR;
+    }
+    flags = fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+    if (flags < 0) {
+        tswWarn("%s", strerror(errno));
+        return TSW_ERR;
+    }
+
+    return TSW_OK;
+}
+
 int tswCo_accept(tswCo_schedule *S, int sockfd, struct sockaddr *addr, socklen_t *addrlen)
 {
     int connfd;
@@ -27,4 +47,74 @@ int tswCo_accept(tswCo_schedule *S, int sockfd, struct sockaddr *addr, socklen_t
 	}
 
     return connfd;
+}
+
+ssize_t tswCo_recv(tswCo_schedule *S, int connfd, void *buf, size_t len, int flags)
+{
+    int connfd_flags;
+    int n = 0;
+    int total = 0;
+    int remain = len;
+
+    connfd_flags = fcntl(connfd, F_GETFL, 0);
+    if (connfd_flags < 0) {
+        tswWarn("%s", strerror(errno));
+        return TSW_ERR;
+    }
+    if (!(connfd_flags & O_NONBLOCK)) { // if is not NONBLOCK
+        if (tswCo_setnonblock(connfd) < 0) {
+            tswWarn("tswCo_setnonblock error");
+            return TSW_ERR;
+        }
+    }
+
+    do {
+        remain -= n;
+        n = recv(connfd, buf + total, remain, flags);
+        if (n < 0 && errno == EAGAIN) {
+            if (tswCo_wait(S, connfd, TSW_FD_WRITE) < 0) {
+                tswWarn("tswCo_wait error");
+                return TSW_ERR;
+            }
+            n = 0;
+        }
+        total += n;
+    } while (total < len);
+
+    return len;
+}
+
+ssize_t tswCo_send(tswCo_schedule *S, int connfd, void *buf, size_t len, int flags)
+{
+    int connfd_flags;
+    int n = 0;
+    int total = 0;
+    int remain = len;
+
+    connfd_flags = fcntl(connfd, F_GETFL, 0);
+    if (connfd_flags < 0) {
+        tswWarn("%s", strerror(errno));
+        return TSW_ERR;
+    }
+    if (!(connfd_flags & O_NONBLOCK)) { // if is not NONBLOCK
+        if (tswCo_setnonblock(connfd) < 0) {
+            tswWarn("tswCo_setnonblock error");
+            return TSW_ERR;
+        }
+    }
+
+    do {
+        remain -= n;
+        n = send(connfd, buf + total, remain, flags);
+        if (n < 0 && errno == EAGAIN) {
+            if (tswCo_wait(S, connfd, TSW_FD_WRITE) < 0) {
+                tswWarn("tswCo_wait error");
+                return TSW_ERR;
+            }
+            n = 0;
+        }
+        total += n;
+    } while (total < len);
+
+    return len;
 }
--- net.h
+++ net.h
@@ -3,8 +3,11 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
 #include "coroutine.h"
 
 int tswCo_accept(tswCo_schedule *S, int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+ssize_t tswCo_recv(tswCo_schedule *S, int connfd, void *buf, size_t len, int flags);
+ssize_t tswCo_send(tswCo_schedule *S, int connfd, void *buf, size_t len, int flags);
 
 #endif /* TSW_NET_H_ */
