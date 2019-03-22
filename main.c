81e540fcf3830859dfb0aa63741058672c917e72
codinghuang <2812240764@qq.com>
2019-03-22 10:51:44 +0800

    Update: add tswCo_main coroutine

--- main.c
+++ main.c
@@ -60,7 +60,7 @@ void listen_service(tswCo_schedule *S, void *ud)
     }
 }
 
-int start_service(tswCo_schedule *S)
+void start_service(tswCo_schedule *S, void *ud)
 {
     int sockfd;
     int on = 1;
@@ -68,26 +68,27 @@ int start_service(tswCo_schedule *S)
     sockfd = tswSocket_create(TSW_SOCK_TCP);
     if (sockfd < 0) {
         tswWarn("tswSocket_create error");
-        return -1;
     }
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
     if (tswSocket_bind(sockfd, TSW_SOCK_TCP, HOST, PORT) < 0) {
         tswWarn("tswSocket_bind error");
-        return -1;
     }
     if (listen(sockfd, LISTENQ) < 0) {
         tswWarn("%s", strerror(errno));
     }
 
     tswCo_create(S, TSW_CO_DEFAULT_ST_SZ, listen_service, (void *)(uintptr_t)sockfd);
-    tswCo_run(S);
+}
+/**
+ * main coroutine
+ * you can define the task coroutine here
+ */
 
-    return 0;
+void tswCo_main(tswCo_schedule *S, void *ud)
+{
+    tswCo_create(S, TSW_CO_DEFAULT_ST_SZ, start_service, (void *)NULL);
 }
 
-/*
- * main coroutine
-*/
 int main(int argc, char const *argv[])
 {
     tswCo_schedule *S;
@@ -97,7 +98,9 @@ int main(int argc, char const *argv[])
         tswWarn("tswCo_open error");
         return -1;
     }
-    start_service(S);
+
+    tswCo_create(S, TSW_CO_DEFAULT_ST_SZ, tswCo_main, (void *)NULL);
+    tswCo_run(S);
     tswCo_destroy(S);
 
     return 0;
